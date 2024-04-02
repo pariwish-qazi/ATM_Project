016ed338dad20c1af2f4a209d477577e42ecadf9
Bisma Qazi <bismaqazi2005@gmail.com>
2024-04-02 06:17:44 +0500

    ATM Project Typescript

---
+++ main.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let theBalance = 80000;
+let myPin = 123;
+
+let pinAnswer = await inquirer.prompt([{
+    name: "pin",
+    type: "number",
+    message: "Enter your pin"
+}]);
+
+if(pinAnswer.pin == myPin)
+{
+    console.log("Correct pin");
+    let operationAns = await inquirer.prompt([{
+        name: "operation",
+        type: "list",
+        message: "Select any one option.",
+        choices: ["Check Balance", "Withdraw"]}]);
+        console.log(operationAns);
+        if (operationAns.operation == "Withdraw") {
+            let withdrawAmount = await inquirer.prompt([{
+                name: "amount",
+                type: "number",
+                message: "Enter amount you want to withdraw"
+            }]);
+            theBalance -= withdrawAmount.amount;
+            console.log("Your remaining balance is"+ theBalance);
+        } 
+        else if (operationAns.operation == "Check Balance") {
+            console.log("Your remaining balance is"+ theBalance);
+        }       
+    }
+    else {
+        console.log("Wrong pin");}
